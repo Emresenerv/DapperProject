2ebec1f4372f6049890c381fa7bc4bf8bde3bba1
AsusTEK <AsusTEK@DESKTOP-6JTJ49H>
2024-07-31 16:48:33 +0300

    Add project files.

---
+++ DapperProject/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using DapperProject.Dtos.ProductDtos;
+using DapperProject.Services;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
+
+namespace DapperProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> ProductList()
+        {
+            var values = await _productService.GetAllProductAsync();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            await _productService.CreateProductAsync(createProductDto);
+            return RedirectToAction("ProductList");
+        }
+        public async Task<IActionResult> ProductListWithCategory()
+        {
+            var values=await _productService.GetAllProductWithCategoryAsync();
+            ViewBag.v = await _productService.GetAllProductAsync();
+            return View(values);
+
+        }
+    }
+}
+
---
+++ DapperProject/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace DapperProject.Dtos.CategoryDtos
+{
+    public class ResultCategoryDto
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
